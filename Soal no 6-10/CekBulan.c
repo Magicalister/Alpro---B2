809fb5c9cb213a2d7624b6ad04b5ee03c6bac0fa
Magicalister <92492590+Magicalister@users.noreply.github.com>
2022-03-12 11:11:32 +0700

    Update CekBulan.c

--- Soal no 6-10/CekBulan.c
+++ Soal no 6-10/CekBulan.c
@@ -2,7 +2,7 @@
 Nama File 	    : CekBulan.c
 Deskripsi 	    : mengecek dan menampilkan bilangan integer N yang telah diklasifikasikan ke nama bulan
 Pembuat   	    : Zhulfani Faisal Adam - 24060121140132
-Tgl Pembuatan	: 10-03-2021
+Tgl Pembuatan	: 10-03-2022
 */
 
 #include <stdio.h>
